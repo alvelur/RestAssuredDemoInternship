54b8710bee5ffe8b5c9a4d16f3ae1030d603d8db
Alejandro Velasquez <alejandro.velasquez@dfinsolutions.com>
2024-11-25 15:36:45 -0500

    First session content

---
+++ src/test/java/pojo/User.java
@@ -0,0 +1,55 @@
+package pojo;
+
+public class User {
+
+    private String name;
+    private String email;
+    private String password;
+    private String avatar;
+
+    /**
+     * No args constructor for use in serialization
+     */
+    public User() {
+    }
+
+    public User(String name, String email, String password, String avatar) {
+        super();
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.avatar = avatar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
